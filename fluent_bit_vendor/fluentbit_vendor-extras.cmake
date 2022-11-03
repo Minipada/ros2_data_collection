d9d55b4893fa0c7d28127d3bbf2638eef713efdc
David Bensoussan <d.bensoussan@proton.me>
2022-11-03 19:32:03 +0100

    feat: add Fluent Bit vendor package
    With this package, we don't need to install Fluent Bit beforehand

---
+++ fluent_bit_vendor/fluentbit_vendor-extras.cmake
@@ -0,0 +1,3 @@
+list(INSERT CMAKE_MODULE_PATH 0 "${fluent_bit_vendor_DIR}/Modules")
+list(INSERT CMAKE_MODULE_PATH 0 "${fluent_bit_vendor_DIR}/cmake")
+list(INSERT CMAKE_MODULE_PATH 0 "${fluent_bit_vendor_DIR}/sanitizers-cmake/cmake")
