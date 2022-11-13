6c5486568c671e6e2caed3f854d6a585b4d9b453
David Bensoussan <d.bensoussan@proton.me>
2022-11-13 19:32:00 +0100

    feat: add bringup package to start measurement as lifecycle node

---
+++ dc_bringup/launch/dc_bringup.launch.py
@@ -0,0 +1,180 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import LoadComposableNodes, SetParameter
+from launch_ros.actions import Node
+from launch_ros.descriptions import ComposableNode
+from nav2_common.launch import RewrittenYaml
+
+
+def generate_launch_description():
+    # Get the launch directory
+    bringup_dir = get_package_share_directory("dc_bringup")
+
+    namespace = LaunchConfiguration("namespace")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    autostart = LaunchConfiguration("autostart")
+    params_file = LaunchConfiguration("params_file")
+    use_composition = LaunchConfiguration("use_composition")
+    container_name = LaunchConfiguration("container_name")
+    use_respawn = LaunchConfiguration("use_respawn")
+    log_level = LaunchConfiguration("log_level")
+
+    lifecycle_nodes = ["measurement_server"]
+
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {"autostart": autostart}
+
+    configured_params = RewrittenYaml(
+        source_file=params_file,
+        root_key=namespace,
+        param_rewrites=param_substitutions,
+        convert_types=True,
+    )
+
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        "RCUTILS_LOGGING_BUFFERED_STREAM", "1"
+    )
+
+    declare_namespace_cmd = DeclareLaunchArgument(
+        "namespace", default_value="", description="Top-level namespace"
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        "params_file",
+        default_value=os.path.join(bringup_dir, "params", "dc_params.yaml"),
+        description="Full path to the ROS2 parameters file to use for all launched nodes",
+    )
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        "autostart",
+        default_value="True",
+        description="Automatically startup the dc stack",
+    )
+
+    declare_use_composition_cmd = DeclareLaunchArgument(
+        "use_composition",
+        default_value="True",
+        description="Use composed bringup if True",
+    )
+
+    declare_container_name_cmd = DeclareLaunchArgument(
+        "container_name",
+        default_value="dc_container",
+        description="the name of container that nodes will load in if use composition",
+    )
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        "use_respawn",
+        default_value="False",
+        description="Whether to respawn if a node crashes. Applied when composition is disabled.",
+    )
+
+    declare_log_level_cmd = DeclareLaunchArgument(
+        "log_level", default_value="info", description="log level"
+    )
+
+    load_nodes = GroupAction(
+        condition=IfCondition(PythonExpression(["not ", use_composition])),
+        actions=[
+            SetParameter("use_sim_time", use_sim_time),
+            Node(
+                package="dc_measurements",
+                executable="measurement_server",
+                name="measurement_server",
+                output={
+                    "stdout": "screen",
+                    "stderr": "screen",
+                },
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=["--ros-args", "--log-level", log_level],
+            ),
+            Node(
+                package="nav2_lifecycle_manager",
+                executable="lifecycle_manager",
+                name="lifecycle_manager_navigation",
+                output={
+                    "stdout": "screen",
+                    "stderr": "screen",
+                },
+                arguments=["--ros-args", "--log-level", log_level],
+                parameters=[
+                    {"autostart": autostart},
+                    {"node_names": lifecycle_nodes, "bond_timeout": 10.0},
+                ],
+            ),
+        ],
+    )
+
+    load_composable_nodes = GroupAction(
+        condition=IfCondition(use_composition),
+        actions=[
+            SetParameter("use_sim_time", use_sim_time),
+            Node(
+                condition=IfCondition(use_composition),
+                name=container_name,
+                package="rclcpp_components",
+                executable="component_container_isolated",
+                parameters=[configured_params, {"autostart": autostart}],
+                arguments=["--ros-args", "--log-level", log_level],
+                output="screen",
+            ),
+            LoadComposableNodes(
+                target_container=container_name,
+                composable_node_descriptions=[
+                    ComposableNode(
+                        package="dc_measurements",
+                        plugin="measurement_server::MeasurementServer",
+                        name="measurement_server",
+                        parameters=[configured_params],
+                    ),
+                    ComposableNode(
+                        package="nav2_lifecycle_manager",
+                        plugin="nav2_lifecycle_manager::LifecycleManager",
+                        name="lifecycle_manager_navigation",
+                        parameters=[
+                            {
+                                "autostart": autostart,
+                                "node_names": lifecycle_nodes,
+                                "bond_timeout": 10.0,
+                            }
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Set environment variables
+    ld.add_action(stdout_linebuf_envvar)
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_composition_cmd)
+    ld.add_action(declare_container_name_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(declare_log_level_cmd)
+    # Add the actions to launch all of the navigation nodes
+    ld.add_action(load_nodes)
+    ld.add_action(load_composable_nodes)
+
+    return ld
