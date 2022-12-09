b613f1ca6c23fa6eb0491da11d6babe1bb41592d
David Bensoussan <d.bensoussan@proton.me>
2022-12-09 19:58:03 +0100

    feat: add Flb file plugin

---
+++ dc_destinations/plugins/flb_file.hpp
@@ -0,0 +1,35 @@
+#ifndef DC_DESTINATIONS__PLUGINS__DESTINATIONS__FLB_FILE_HPP_
+#define DC_DESTINATIONS__PLUGINS__DESTINATIONS__FLB_FILE_HPP_
+
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "dc_destinations/flb_destination.hpp"
+#include "dc_destinations/destination_server.hpp"
+
+namespace dc_destinations
+{
+
+class Flbfile : public dc_destinations::FlbDestination
+{
+public:
+  Flbfile();
+  ~Flbfile();
+
+protected:
+  /**
+   * @brief Configuration of behavior action
+   */
+  void initFlbOutputPlugin() override;
+  void onConfigure() override;
+
+  std::string path_;
+  std::string file_;
+  std::string format_;
+  std::string mkdir_;
+};
+
+}  // namespace dc_destinations
+
+#endif  // DC_DESTINATIONS__PLUGINS__DESTINATIONS__FLB_FILE_HPP_
