83f3792e68582c1213684bb6ba460262f3faac01
David Bensoussan <d.bensoussan@proton.me>
2022-12-26 19:22:57 +0100

    feat: add service to save image

---
+++ dc_services/launch/dc_save_image.launch.py
@@ -0,0 +1,26 @@
+"""Launch minimal publisher with default configuration."""
+import os
+
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description() -> launch.LaunchDescription:
+    """Launch file generator.
+
+    Returns:
+        launch.LaunchDescription: Launch
+    """
+
+    barcode_node = launch_ros.actions.Node(
+        package="dc_services",
+        executable="save_image",
+        namespace="/dc/service",
+        output={
+            "stdout": "screen",
+            "stderr": "screen",
+        },
+    )
+
+    nodes = [barcode_node]
+    return launch.LaunchDescription(nodes)
