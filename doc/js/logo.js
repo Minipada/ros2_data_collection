66acd23339a208839d40b1c46f5f11bed85c3b85
David Bensoussan <d.bensoussan@proton.me>
2023-02-14 19:06:11 +0100

    feat: add slack destination plugin

---
+++ doc/js/logo.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+    console.log("ok")
+    let navbar = document.getElementById('sidebar');
+    var logo_dc = document.createElement("img");
+    logo_dc.src = "/images/dc.png"
+    logo_dc.id = 'logo-dc';
+    navbar.prepend(logo_dc);
+});
