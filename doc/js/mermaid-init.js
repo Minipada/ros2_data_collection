17327824540525cd7ea8c0b171dafb74b671e220
David Bensoussan <d.bensoussan@proton.me>
2023-03-01 19:06:13 +0100

    fix: mermaid color is loaded correctly

--- doc/js/mermaid-init.js
+++ doc/js/mermaid-init.js
@@ -3,13 +3,12 @@ function get_theme_mermaid() {
 
     let mdbook_theme = localStorage.getItem('mdbook-theme')
 
-    if (mdbook_theme == "ayu" || mdbook_theme == "navy" || mdbook_theme == "coal") {
+    if (mdbook_theme == "ayu" || mdbook_theme == "navy" || mdbook_theme == "coal" || mdbook_theme == undefined) {
+        // If undefined it is still dark, since default is navy mode, defined in book.toml
         mermaid_theme = "dark";
     }
     return mermaid_theme
 }
-
-mermaid.initialize({ startOnLoad: true, theme: get_theme_mermaid() });
 
 // To reload mermaid graphs
 document.getElementById('light').onclick = function () {
@@ -31,3 +30,5 @@ document.getElementById('navy').onclick = function () {
 document.getElementById('ayu').onclick = function () {
     location.reload();
 };
+
+mermaid.initialize({ startOnLoad: true, theme: get_theme_mermaid() });
