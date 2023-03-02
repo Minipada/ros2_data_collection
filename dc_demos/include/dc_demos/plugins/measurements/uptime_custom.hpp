2c64078d0a0286d6ddce4a95e7db436ceb84a44e
David Bensoussan <david.bensoussan@brisa.tech>
2023-03-02 03:27:43 +0100

    feat: measurement callback with failed validation has access to json data

--- dc_demos/include/dc_demos/plugins/measurements/uptime_custom.hpp
+++ dc_demos/include/dc_demos/plugins/measurements/uptime_custom.hpp
@@ -1,16 +1,19 @@
 #ifndef DC_DEMOS__PLUGINS__MEASUREMENTS__UPTIME_CUSTOM_HPP_
 #define DC_DEMOS__PLUGINS__MEASUREMENTS__UPTIME_CUSTOM_HPP_
 
+#include <nlohmann/json.hpp>
+
 #include "dc_measurements/measurement.hpp"
 #include "dc_measurements/plugins/measurements/uptime.hpp"
 
 namespace dc_demos
 {
+using json = nlohmann::json;
 
 class UptimeCustom : public dc_measurements::Uptime
 {
 protected:
-  void onFailedValidation() override;
+  void onFailedValidation(json data_json) override;
   void setValidationSchema() override;
 };
 
