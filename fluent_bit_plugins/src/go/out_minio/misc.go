81b70eaa3539d59eca3a71d27b08c6380f52461b
David Bensoussan <david.bensoussan@brisa.tech>
2023-03-26 00:17:29 +0100

    fix: destination flb-minio now matches different groups depending on record

---
+++ fluent_bit_plugins/src/go/out_minio/misc.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func NestedMapLookup(m map[interface{}]interface{}, ks ...string) (rval interface{}, err error) {
+	var ok bool
+
+	if len(ks) == 0 { // degenerate input
+		return nil, fmt.Errorf("NestedMapLookup needs at least one key")
+	}
+	if rval, ok = m[ks[0]]; !ok {
+		return nil, fmt.Errorf("key not found; remaining keys: %v", ks)
+	} else if len(ks) == 1 { // we've reached the final key
+		return rval, nil
+	} else if m, ok = rval.(map[interface{}]interface{}); !ok {
+		return nil, fmt.Errorf("malformed structure at %#v", rval)
+	} else { // 1+ more keys
+		return NestedMapLookup(m, ks[1:]...)
+	}
+}
+
+func get_file_content(path string) string {
+	file, err := os.Open(path)
+
+	defer file.Close()
+
+	if err != nil {
+	   panic(err)
+	}
+	// Get the file content
+	buf := make([]byte, 512)
+	_, err = file.Read(buf)
+
+	if err != nil {
+		panic(err)
+	}
+
+	contentType := http.DetectContentType(buf)
+
+	return contentType
+}
+
+func contains(elems []int, v int) bool {
+	for _, s := range elems {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
