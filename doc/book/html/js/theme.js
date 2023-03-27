05c5e0b4b91c018b6f5e5d507de9d4459cb3624e
David Bensoussan <david.bensoussan@brisa.tech>
2023-03-27 02:47:42 +0200

    doc: add book

---
+++ doc/book/html/js/theme.js
@@ -0,0 +1,20 @@
+// To reload mermaid graphs
+document.getElementById('light').onclick = function () {
+    location.reload();
+};
+
+document.getElementById('rust').onclick = function () {
+    location.reload();
+};
+
+document.getElementById('coal').onclick = function () {
+    location.reload();
+};
+
+document.getElementById('navy').onclick = function () {
+    location.reload();
+};
+
+document.getElementById('ayu').onclick = function () {
+    location.reload();
+};
