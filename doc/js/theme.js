2cb9cd3fb6e54d233de39e3cbc4e5caba731db2c
David Bensoussan <david.bensoussan@brisa.tech>
2023-03-27 16:58:34 +0200

    fix+doc: change the url of the logo dynamically

--- doc/js/theme.js
+++ doc/js/theme.js
@@ -18,3 +18,11 @@ document.getElementById('navy').onclick = function () {
 document.getElementById('ayu').onclick = function () {
     location.reload();
 };
+
+if (window.location.href.indexOf('http://localhost/') == 0) {
+    document.getElementById("logo-dc").src = "/images/dc.png"
+} else if (window.location.href.indexOf('https://minipada.github.io/ros2_data_collection/') == 0) {
+    let level = window.location.href.split("https://minipada.github.io/ros2_data_collection/")[1].split("/").length - 1
+
+    document.getElementById("logo-dc").src = "../".repeat(level) + "images/dc.png"
+}
