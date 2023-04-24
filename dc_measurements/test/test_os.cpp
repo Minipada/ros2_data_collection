4be3f12ba53eeb708f69b1bfda7605f141df118e
David Bensoussan <d.bensoussan@proton.me>
2023-04-24 23:23:46 +0200

    cleanup

--- dc_measurements/test/test_os.cpp
+++ dc_measurements/test/test_os.cpp
@@ -6,15 +6,15 @@
 #include "dc_interfaces/msg/string_stamped.hpp"
 #include "dc_measurements/measurement_server.hpp"
 #include "dc_util/json_utils.hpp"
-class MeasurementServerTest : public ::testing::Test
+class MeasurementOSTest : public ::testing::Test
 {
 protected:
-  MeasurementServerTest()
+  MeasurementOSTest()
   {
     SetUp();
   }
 
-  ~MeasurementServerTest() override
+  ~MeasurementOSTest() override
   {
   }
 
@@ -24,7 +24,7 @@ protected:
                                                                        std::vector<std::string>{ "os" });
     sub_data_ = ms_node_->create_subscription<dc_interfaces::msg::StringStamped>(
         "/dc/measurement/os", rclcpp::SystemDefaultsQoS(),
-        std::bind(&MeasurementServerTest::osDataCallback, this, std::placeholders::_1));
+        std::bind(&MeasurementOSTest::osDataCallback, this, std::placeholders::_1));
   }
 
   void TearDown() override
@@ -41,10 +41,6 @@ protected:
 
   void osDataCallback(const dc_interfaces::msg::StringStamped& msg)
   {
-    // std::string yaml_str = dc_interfaces::msg::to_yaml(msg);
-    // YAML::Node yaml_node = YAML::Load(yaml_str);
-    // nlohmann::json data_json = dc_util::tojson::detail::yaml2json(yaml_node);
-
     std::string data_str = msg.data.c_str();
     boost::replace_all(data_str, "'", "\"");
     nlohmann::json data_json = nlohmann::json::parse(data_str);
@@ -63,7 +59,7 @@ public:
   bool os_callback_{ false };
 };
 
-TEST_F(MeasurementServerTest, ParametersSaved)
+TEST_F(MeasurementOSTest, ParametersSaved)
 {
   ms_node_->declare_parameter("os.plugin", std::string("dc_measurements/OS"));
   ms_node_->declare_parameter("os.group_key", std::string("os"));
@@ -79,7 +75,7 @@ TEST_F(MeasurementServerTest, ParametersSaved)
   EXPECT_EQ(ms_group_key_desired, ms_node_->getMeasurementGroupKeys());
 }
 
-TEST_F(MeasurementServerTest, OSDataCorrect)
+TEST_F(MeasurementOSTest, OSDataCorrect)
 {
   ms_node_->declare_parameter("os.plugin", std::string("dc_measurements/OS"));
   ms_node_->declare_parameter("os.group_key", std::string("os"));
