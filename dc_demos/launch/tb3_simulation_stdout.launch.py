d6b3d160d8bf0fd7a85d99a5693604d804a59ae9
David Bensoussan <d.bensoussan@proton.me>
2023-05-22 17:53:14 +0200

    conf: update to reflect custom_params changes (#191)
    Signed-off-by: David Bensoussan <d.bensoussan@proton.me>

--- dc_demos/launch/tb3_simulation_stdout.launch.py
+++ dc_demos/launch/tb3_simulation_stdout.launch.py
@@ -69,7 +69,7 @@ def generate_launch_description():
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         "use_sim_time",
-        default_value="True",
+        default_value="False",
         description="Use simulation (Gazebo) clock if true",
     )
 
