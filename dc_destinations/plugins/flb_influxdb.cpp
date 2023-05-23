fcdf68a336908ad851447902ec36657db764d99e
David Bensoussan <d.bensoussan@proton.me>
2023-05-23 18:08:53 +0200

    feat: add influxdb destination (#193)
    Signed-off-by: David Bensoussan <d.bensoussan@proton.me>

---
+++ dc_destinations/plugins/flb_influxdb.cpp
@@ -0,0 +1,76 @@
+#include "dc_destinations/plugins/flb_influxdb.hpp"
+
+namespace dc_destinations
+{
+
+FlbInfluxDB::FlbInfluxDB() : dc_destinations::FlbDestination()
+{
+}
+
+void FlbInfluxDB::onConfigure()
+{
+  auto node = getNode();
+
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".host", rclcpp::ParameterValue("127.0.0.1"));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".port", rclcpp::ParameterValue(8086));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".database",
+                                               rclcpp::ParameterValue("fluentbit"));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".bucket", rclcpp::ParameterValue(""));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".org", rclcpp::ParameterValue("fluent"));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".sequence_tag",
+                                               rclcpp::ParameterValue("_seq"));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".http_user", rclcpp::ParameterValue(""));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".http_password", rclcpp::ParameterValue(""));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".http_token", rclcpp::ParameterValue(""));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".tag_keys",
+                                               rclcpp::ParameterValue(std::vector<std::string>()));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".auto_tags", rclcpp::ParameterValue(false));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".tags_list_enabled",
+                                               rclcpp::ParameterValue(false));
+  nav2_util::declare_parameter_if_not_declared(node, destination_name_ + ".tags_list_key",
+                                               rclcpp::ParameterValue("tags"));
+  node->get_parameter(destination_name_ + ".host", host_);
+  node->get_parameter(destination_name_ + ".port", port_);
+  node->get_parameter(destination_name_ + ".database", database_);
+  node->get_parameter(destination_name_ + ".bucket", bucket_);
+  node->get_parameter(destination_name_ + ".org", org_);
+  node->get_parameter(destination_name_ + ".sequence_tag", sequence_tag_);
+  node->get_parameter(destination_name_ + ".http_user", http_user_);
+  node->get_parameter(destination_name_ + ".http_password", http_password_);
+  node->get_parameter(destination_name_ + ".http_token", http_token_);
+  node->get_parameter(destination_name_ + ".tag_keys", tag_keys_);
+  node->get_parameter(destination_name_ + ".auto_tags", auto_tags_);
+  node->get_parameter(destination_name_ + ".tags_list_enabled", tags_list_enabled_);
+  node->get_parameter(destination_name_ + ".tags_list_key", tags_list_key_);
+}
+
+void FlbInfluxDB::initFlbOutputPlugin()
+{
+  out_ffd_ = flb_output(ctx_, "influxdb", NULL);
+  flb_output_set(ctx_, out_ffd_, "match", destination_name_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "host", host_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "port", std::to_string(port_), NULL);
+  flb_output_set(ctx_, out_ffd_, "database", database_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "bucket", bucket_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "org", org_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "sequence_tag", sequence_tag_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "http_user", http_user_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "http_password", http_password_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "http_token", http_token_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "tag_keys", tag_keys_.c_str(), NULL);
+  flb_output_set(ctx_, out_ffd_, "auto_tags", dc_util::boolToString(auto_tags_), NULL);
+  flb_output_set(ctx_, out_ffd_, "tags_list_enabled", dc_util::boolToString(tags_list_enabled_), NULL);
+  flb_output_set(ctx_, out_ffd_, "tags_list_key", dc_util::join(tags_list_key_, " "), NULL);
+  if (out_ffd_ == -1)
+  {
+    flb_destroy(ctx_);
+    throw std::runtime_error("Cannot initialize Fluent Bit output influxdb plugin");
+  }
+}
+
+FlbInfluxDB::~FlbInfluxDB() = default;
+
+}  // namespace dc_destinations
+
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(dc_destinations::FlbInfluxDB, dc_core::Destination)
---
+++ doc/src/dc/destinations/flb_influxdb.md
@@ -0,0 +1,36 @@
+# InfluxDB - Fluent Bit
+
+## Description
+
+The influxdb output plugin, allows to flush your records into a [InfluxDB](https://www.influxdata.com/time-series-platform/influxdb/) time series database. The following instructions assumes that you have a fully operational InfluxDB service running in your system. See [fluent bit page](https://docs.fluentbit.io/manual/pipeline/outputs/influxdb) for more information.
+
+## Parameters
+
+| Parameter             | Description                                                                                                    | Type        | Default        |
+| --------------------- | -------------------------------------------------------------------------------------------------------------- | ----------- | -------------- |
+| **host**              | IP address or hostname of the target InfluxDB service.                                                         | str         | "127.0.01"     |
+| **port**              | TCP port of the target InfluxDB service.                                                                       | int         | 8086           |
+| **database**          | InfluxDB database name where records will be inserted.                                                         | str         | "fluentbit"    |
+| **bucket**            | InfluxDB bucket name where records will be inserted - if specified, database is ignored and v2 of API is used. | str         | N/A (Optional) |
+| **org**               | InfluxDB organization name where the bucket is (v2 only).                                                      | str         | "fluent"       |
+| **sequence_tag**      | The name of the tag whose value is incremented for the consecutive simultaneous events.                        | str         | "_seq"         |
+| **http_user**         | Optional username for HTTP Basic Authentication.                                                               | str         | N/A (Optional) |
+| **http_passwd**       | Password for user defined in HTTP_User.                                                                        | str         | N/A (Optional) |
+| **http_token**        | Authentication token used with InfluDB v2 - if specified, both HTTP_User and HTTP_Passwd are ignored.          | str         | N/A (Optional) |
+| **tag_keys**          | List of keys that needs to be tagged.                                                                          | list\[str\] | N/A (Optional) |
+| **auto_tags**         | Automatically tag keys where value is string.                                                                  | bool        | false          |
+| **tags_list_enabled** | Dynamically tag keys which are in the string array at Tags_List_Key key.                                       | bool        | false          |
+| **tags_list_key**     | Key of the string array optionally contained within each log record that contains tag keys for that record.    | str         | "tags"         |
+
+## Node configuration
+
+```yaml
+...
+flb_influxdb:
+  plugin: "dc_destinations/FlbInfluxDB"
+  inputs: ["/dc/measurement/uptime"]
+  host: "127.0.0.1"
+  port: 8086
+  database: "ros"
+...
+```
